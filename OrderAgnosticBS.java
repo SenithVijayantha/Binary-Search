20d33e71496de6b89775782861f74d5ea4ba463f
Master Senith <55469468+SenithVijayantha@users.noreply.github.com>
2024-01-01 12:19:35 +0530

    Create OrderAgnosticBS.java
    Implemented Order-Agnostic Binary Search in Java: Added a Java program that performs binary search on a sorted array regardless of whether it is in ascending or descending order. The code includes modularized functions for handling both order scenarios, user input handling, and display of the index and corresponding array element for the target integer.

---
+++ OrderAgnosticBS.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class OrderAgnosticBS {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the integer you want to search for: ");
+        int target = in.nextInt();
+        in.close();
+
+        int[] arr = {20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int ans = orderAgnosticBinarySearch(arr, target);
+        System.out.println(ans);
+        System.out.println(arr[ans]);
+    }
+
+    static int orderAgnosticBinarySearch(int[] arr, int target) {
+
+        // will return -1 if the target was not found 
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        // Determines whether the array is in descending order.
+        if (arr[start] > arr[end]) {
+            while (start <= end) {
+                // int mid = (start + end) / 2 - Might be possible that (start + end) exceeds the range of int in Java
+                int mid = start + (end - start) / 2;
+
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else if (target < arr[mid]) {
+                    start = mid + 1;
+                } else {
+                    return mid;
+                }
+            }
+        } else { // Determines whether the array is in ascending order.
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+
+                if (target > arr[mid]) {
+                    start = mid + 1;
+                } else if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    return mid;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
